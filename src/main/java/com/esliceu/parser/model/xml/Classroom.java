5adc3dc07c3c723e33af722cd0b74714eb1bc812
Javier Rotger <fjrotgerl@gmail.com>
2019-02-27 18:13:53 +0100

    clase classroom implementada

--- src/main/java/com/esliceu/parser/model/xml/Classroom.java
+++ src/main/java/com/esliceu/parser/model/xml/Classroom.java
@@ -5,70 +5,29 @@ import javax.xml.bind.annotation.*;
 import java.util.List;
 
 
-@XmlRootElement(name = "CENTRE_EXPORT")
+@XmlRootElement(name = "AULA")
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlSeeAlso({ Teacher.class,Student.class, ScheduleStudents.class, ScheduleTeachers.class, Group.class })
 public class Classroom {
 
-    @XmlElement(name="PROFESSORS")
-    private List<Teachers> professors;
+    @XmlElement(name="codi")
+    private Integer codi;
 
-    @XmlElement(name="ALUMNES")
-    private List<Students> alumnes;
+    @XmlElement(name="descripcio")
+    private String descripcio;
 
-    @XmlElement(name="HORARIP")
-    private List<ScheduleTeachers> scheduleTeachers;
-
-    @XmlElement(name="HORARIA")
-    private List<ScheduleStudents> scheduleStudents;
-
-    @XmlElement(name="CURSOS")
-    private List<Courses> courses;
-
-
-
-
-    public List<ScheduleTeachers> getScheduleTeachers() {
-        return scheduleTeachers;
+    public Integer getCodi() {
+        return codi;
     }
 
-    public void setScheduleTeachers(List<ScheduleTeachers> scheduleTeachers) {
-        this.scheduleTeachers = scheduleTeachers;
+    public void setCodi(Integer codi) {
+        this.codi = codi;
     }
 
-
-    public List<ScheduleStudents> getScheduleStudents() {
-        return scheduleStudents;
+    public String getDescripcio() {
+        return descripcio;
     }
 
-    public void setScheduleStudents(List<ScheduleStudents> scheduleStudents) {
-        this.scheduleStudents = scheduleStudents;
+    public void setDescripcio(String descripcio) {
+        this.descripcio = descripcio;
     }
-
-    public List<Students> getAlumnes() {
-        return alumnes;
-    }
-
-    public void setAlumnes(List<Students> alumnes) {
-        this.alumnes = alumnes;
-    }
-
-    public List<Courses> getCourses() {
-        return courses;
-    }
-
-    public void setCourses(List<Courses> courses) {
-        this.courses = courses;
-    }
-
-    public List<Teachers> getProfessors() {
-        return professors;
-    }
-
-    public void setProfessors(List<Teachers> professors) {
-        this.professors = professors;
-    }
-
-
-    //HORARIA
 }
