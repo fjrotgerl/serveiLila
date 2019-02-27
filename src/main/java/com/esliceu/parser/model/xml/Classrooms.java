cd45f2a80cca72684401167df89f95b63a9abecd
Javier Rotger <fjrotgerl@gmail.com>
2019-02-27 18:14:09 +0100

    clase classrooms creada y implementada

---
+++ src/main/java/com/esliceu/parser/model/xml/Classrooms.java
@@ -0,0 +1,22 @@
+package com.esliceu.parser.model.xml;
+
+import javax.xml.bind.annotation.*;
+import java.util.List;
+
+@XmlRootElement(name="AULES")
+@XmlSeeAlso({Classroom.class})
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Classrooms {
+
+    @XmlElement(name="AULA")
+    private List<Classroom> classrooms;
+
+
+    public List<Classroom> getClassrooms() {
+        return classrooms;
+    }
+
+    public void setClassrooms(List<Classroom> classrooms) {
+        this.classrooms = classrooms;
+    }
+}
